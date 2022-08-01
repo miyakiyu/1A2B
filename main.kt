347e13c56ad950b5a79cd483a54fa42157151629
miyakiyu <miyakiyu>
2022-08-01 22:20:20 +0800

    Add some function
    Add check_a, check_b ,and check_repeat if the numbers have repeat
    number

---
+++ main.kt
@@ -0,0 +1,34 @@
+//import kotlin.random.Random
+
+fun main()
+{
+    //first pick a random number from 1111 to 9999
+    val random_numbers = (1111..9999).random()
+    println("The number is ready! Let's have fun!(｢･ω･)｢")
+    
+    //check if the number have repeat or not, e.g:1111,1231...
+    check_repeat(random_numbers)
+
+    //check 'B'
+    check_b(random_numbers)
+
+    //check 'A'
+    check_a(random_numbers)
+
+    //count the 
+}
+
+fun check_repeat(number:Int)
+{
+
+}
+
+fun check_b(number:Int)
+{
+
+}
+
+fun check_a(number:Int)
+{
+
+}
