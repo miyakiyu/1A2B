c20b53832f4120db4119e90c2c317b46e86f6d09
miyakiyu <miyakiyu>
2022-08-05 19:28:25 +0800

    Fixing the input bug
    Fixing the input bug for List<String> except String

--- main.kt
+++ main.kt
@@ -2,27 +2,28 @@ import java.util.Scanner
 
 fun main()
 {
-    //first pick a random number from 1111 to 9999
-    val random_numbers = listOf(0,1,2,3,4,5,6,7,8,9)
+    //first pick a random number from 0 - 9 
+    val random_numbers = listOf("0","1","2","3","4","5","6","7","8","9")
     val none_repeat_number = random_numbers.shuffled().take(4)
-    println("The number is ready! Let's have fun!(｢･ω･)｢")
+    println("The number is ready! Let's have fun!(｢･ω ･)｢")
 
     //get answer and store it to a List<Int> type
     println("Enter your answer:")
-    val read = Scanner(System.`in`)
-    val your_answer:MutableList<Int> = mutableListOf()
-    val input = readLine()!!.toInt()
-    your_answer.add(input)    
+    val input = readLine()!!.split(" ") //your input
+    val your_answer:MutableList<String> = mutableListOf() //create a list to store your input
+    input.forEach() //put it inside the list
+    {
+        your_answer.add(it)
+    }
 
 
     //check your answer for measure "A" and "B" and print it out
-    //check_ab(none_repeat_number,your_answer)
     val (count_a,count_b) = check_ab(none_repeat_number,your_answer)
     println("You got ${count_a}A ${count_b}B")
 
 }
 
-fun check_ab(number:List<Int>,your_number:List<Int>):Pair<Int,Int>
+fun check_ab(number:List<String>,your_number:List<String>):Pair<Int,Int>
 {
     var count_a:Int = 0
     var count_b:Int = 0
